9bd336aa21e5f18b366c4b8315c05b6cfd638946
ryusuke06 <or.06.012@gmail.com>
2020-09-15 22:45:33 +0900

    func

--- A_Tour_of_GO/2.func.go
+++ A_Tour_of_GO/2.func.go
@@ -12,9 +12,17 @@ func swap(x, y string) (string, string) {
 }
 // 複数の返り値が戻ってくる場合(データ型, データ型)で定義
 
+func split(sum int) (x, y int) { // 引数に名前(ここではsum)と名付けることができる
+	x = sum * 4 / 9
+	y = sum - x
+	return // 引数の変数を使うとreturnステートメントに何も書かなくて良い（naked return）
+}
+
 func main() {
 	fmt.Println(add(42, 13)) // 55
 
 	a, b := swap("hello", "world")
 	fmt.Println(a, b) // world hello
+
+	fmt.Println(split(17))
 }
